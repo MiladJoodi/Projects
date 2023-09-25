30ab50dde1e333b28ef32bbc80b6fdf8d1040067
Milad Joodi <winbeta.com@gmail.com>
2023-09-25 03:41:09 +0330

    0

--- Dashboard-Persian/src/App.js
+++ Dashboard-Persian/src/App.js
@@ -1,19 +1,17 @@
-import './App.css';
-import Home from './pages/Home/Home'
-import UserList from './pages/Users/UserList'
-import routes from './routes'
-import { useRoutes } from 'react-router-dom';
-import Topbar from './components/Topbar/Topbar'
+import "./App.css";
+import Home from "./pages/Home/Home";
+import UserList from "./pages/Users/UserList";
+import routes from "./routes";
+import { useRoutes } from "react-router-dom";
+import Topbar from "./components/Topbar/Topbar";
 
 function App() {
-
-  let router = useRoutes(routes)
+  let router = useRoutes(routes);
 
   return (
     <>
-    <></>
-    
-    {router}
+      <Topbar />
+      {router}
     </>
   );
 }
--- Dashboard-Persian/src/components/Topbar/Topbar.jsx
+++ Dashboard-Persian/src/components/Topbar/Topbar.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
-import './Topbar.css'
+import React from "react";
+import "./Topbar.css";
+import { IoSettingsOutline } from "react-icons/io5";
+import { MdPowerSettingsNew } from "react-icons/md";
+import { IoNotificationsOutline } from "react-icons/io5";
+import IconButton from "@mui/material/IconButton";
+import { GrShareOption } from "react-icons/gr";
+
 
 export default function Topbar() {
   return (
-    <div>topbar</div>
-  )
+    <div className="topbar">
+      <div className="topbarWrapper">
+        {/* topbar-left */}
+        <div className="topbarRight">
+        <GrShareOption className="topbarTitleIcon" />
+          <span className="topbarTitle">پنل کاربری </span>
+        </div>
+        {/* topbar-left */}
+        <div className="topbarLeft">
+          <IconButton aria-label="fingerprint" color="gray">
+            <MdPowerSettingsNew className="topbarIcon" />
+          </IconButton>
+          <IconButton aria-label="fingerprint" color="gray">
+            <span className="topbarBadge">2</span>
+            <IoNotificationsOutline className="topbarIcon" />
+          </IconButton>
+          <IconButton aria-label="fingerprint" color="gray">
+            <IoSettingsOutline className="topbarIcon" />
+          </IconButton>
+          <img src="/images/Steve-Jobs.jpg" className="topbarImg" />
+        </div>
+      </div>
+    </div>
+  );
 }
