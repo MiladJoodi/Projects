252785a123b793da545d02ad932a0c6b617b4571
Milad Joodi <winbeta.com@gmail.com>
2023-09-27 00:39:58 +0330

    Update WidgetLg.jsx

--- Dashboard-Persian/src/components/WidgetLg/WidgetLg.jsx
+++ Dashboard-Persian/src/components/WidgetLg/WidgetLg.jsx
@@ -6,10 +6,23 @@ export default function WidgetLg() {
       <h3 className="widgetLgTitle">آخرین تراکنش ها</h3>
       <table className="widgetLgTable">
         <tr className="widgetLgTr">
-          <th className="widgetLgTh">Customer</th>
-          <th className="widgetLgTh">Date</th>
-          <th className="widgetLgTh">Amount</th>
-          <th className="widgetLgTh">Status</th>
+          <th className="widgetLgTh">نام مشتری</th>
+          <th className="widgetLgTh">تاریخ</th>
+          <th className="widgetLgTh">مبلغ</th>
+          <th className="widgetLgTh">وضعیت</th>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img src="images/profile3" />
+            <span className="widgetLgName">حمیدرضا سرفراز</span>
+          </td>
+          <td className="widgetLgDate persianNumber">
+            4 مهر 1402
+          </td>
+          <td className="widgetLgAmount">
+                475,000 تومان
+          </td>
+          <td className="widgetLgStatus"></td>
         </tr>
       </table>
     </div>
