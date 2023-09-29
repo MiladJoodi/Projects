a55376c3e85e8538c9fe0e4232c88334f57beae9
Milad Joodi <winbeta.com@gmail.com>
2023-09-29 13:17:02 +0330

    start currency

--- Dashboard-Persian/src/pages/exercise/Exercise.jsx
+++ Dashboard-Persian/src/pages/exercise/Exercise.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import React from "react";
 import { useEffect, useState } from "react";
-import useUpdateLogger from './../../Hooks/useUpdateLogger'
-import useCounter from './../../Hooks/useCounter'
-import './Exercise.css'
+import useUpdateLogger from "./../../Hooks/useUpdateLogger";
+import useCounter from "./../../Hooks/useCounter";
+import "./Exercise.css";
 
 export default function Exercise() {
-  // const [value, setValue] = useUpdateLogger('')
-  const [count,add, minus] = useCounter(0)
+  const [posts, setPosts] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((datas) => {
+        console.log(datas);
+        setPosts(datas);
+        setIsPending(false);
+        setError(null);
+      });
+  }, []);
 
   return (
     <div className="exercise">
-
-      {count}
-      <button onClick={add}>add</button>
-      <button onClick={minus}>minus</button>
-      
-      {/* <input
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      /> */}
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {posts &&
+        posts.map(post => (
+          <>
+            <h2>{post.title}</h2>
+            <hr />
+          </>
+        ))}
     </div>
-  )
+  );
 }
