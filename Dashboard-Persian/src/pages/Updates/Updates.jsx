7214c8b9d737ec076882d20d45d868e911f83b6d
Milad Joodi <winbeta.com@gmail.com>
2023-10-01 21:00:01 +0330

    Update Updates.jsx

--- Dashboard-Persian/src/pages/Updates/Updates.jsx
+++ Dashboard-Persian/src/pages/Updates/Updates.jsx
@@ -8,17 +8,26 @@ import './Updates.css'
 export default function Updates() {
   
     const [todos, setTodos] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
+    const [paginatedTodos,setPaginatedTodos] = useState([])
     // const [isPending, setIsPending] = useState(true)
+
+    let pageSize = 10
+    let pagesNumbers;
 
     useEffect(()=>{
       fetch('https://jsonplaceholder.typicode.com/comments')
       .then(res=> res.json())
       .then(datas=> {
-        setTodos(datas)
+        setTodos(datas);
+
+        const pagesCount = Math.ceil(datas.length / pageSize)
+        pagesNumbers = Array.from(Array(pagesCount).keys())
+        console.log(pagesNumbers);
         // setIsPending(false)
         // console.log(datas);
       })
-    })
+    },[])
 
   return (
     <div className="updates">
@@ -53,8 +62,18 @@ export default function Updates() {
             </tbody>
           </table>
         )}
+
+<nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav>
+
         </div>
-      
     </div>
   )
 }
