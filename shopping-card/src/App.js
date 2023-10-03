cbedab902b52a1f869684dfa5027441363d18410
Milad Joodi <winbeta.com@gmail.com>
2023-10-04 01:09:11 +0330

    0

--- shopping-card/src/App.js
+++ shopping-card/src/App.js
@@ -1,20 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar'
-import ProductsSection from './Components/ProductsSection/ProductsSection'
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import ProductsSection from "./Components/ProductsSection/ProductsSection";
+import products from "./products";
+import { useState } from "react";
 
 function App() {
+  const [allProducts, setAllProducts] = useState(products);
+
   return (
     <div className="App">
       <Navbar />
 
-      <main className='pb-5'>
+      <main className="pb-5">
         <div className="container">
-          <h1 className='text-center main-title'>All Products</h1>
-          <ProductsSection title="Phones" />
-          <ProductsSection title="Laptops" />
-          <ProductsSection title="Monitors" />
-          <ProductsSection title="Coolpads" />
+          <h1 className="text-center main-title">All Products</h1>
+          {allProducts.map((product) => (
+            <ProductsSection {...product} />
+          ))}
         </div>
       </main>
     </div>
--- shopping-card/src/products.js
+++ shopping-card/src/products.js
@@ -2,7 +2,16 @@ const products = [
     {
         title: 'Phone',
         infos: [
-            {id: 1, img: '/images/1.jpg', price: 12_000_000}
+            {id: 1, title: 'Samsung A53', img: '/images/2.jpg', price: 12_000_000, count: 34},
+            {id: 2, title: 'Nokia Xz3', img: '/images/1.jpg', price: 2_000_000, count: 120},
+            {id: 3, title: 'Samsung A72', img: '/images/3.jpg', price: 14_000_000, count: 9},
+        ]
+    },
+    {
+        title: 'Laptops',
+        infos: [
+            {id: 1, title: 'Acer Aspire', img: '/images/4.jpg', price: 4_000_000, count: 18},
+            {id: 2, title: 'MacBook Pro M1', img: '/images/5.jpg', price: 45_000_000, count: 12},
         ]
     },
 
