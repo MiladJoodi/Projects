047845d9c9fe8c4d03522847f1e0001753d56a39
Milad Joodi <winbeta.com@gmail.com>
2023-10-03 21:48:43 +0330

    foldername

---
+++ shopping-card/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='navbar navbar-expand-sm py-3 d-flex'>
+        <div className="container">
+            <a href="#" className="navbar-brand">
+                Welcome
+            </a>
+
+            <ul className="navbar-nav me-auto-ms-3">
+                <li className='nav-item'>
+                    <a href="" className='nav-link'>
+                        آHome
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
