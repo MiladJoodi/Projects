daf517e30401c31be43ced6f00117c9aef7a7bba
Milad Joodi <winbeta.com@gmail.com>
2023-10-04 00:32:35 +0330

    products data

--- shopping-card/src/App.js
+++ shopping-card/src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar'
+import ProductsSection from './Components/ProductsSection/ProductsSection'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
+      <main className='pb-5'>
+        <div className="container">
+          <h1 className='text-center main-title'>All Products</h1>
+          <ProductsSection title="Phones" />
+          <ProductsSection title="Laptops" />
+          <ProductsSection title="Monitors" />
+          <ProductsSection title="Coolpads" />
+        </div>
+      </main>
     </div>
   );
 }
---
+++ shopping-card/src/products.js
@@ -0,0 +1,12 @@
+const products = [
+    {
+        title: 'Phone',
+        infos: [
+            {id: 1, img: '/images/1.jpg', price: 12_000_000}
+        ]
+    },
+
+
+]
+
+export default products;
