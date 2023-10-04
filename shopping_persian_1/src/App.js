ad508589acb153fd4a8429ae49122105e6f83f69
Milad Joodi <winbeta.com@gmail.com>
2023-10-05 00:41:57 +0330

    start persian shop

---
+++ shopping_persian_1/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './Component/Navbar/Navbar'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ shopping_persian_1/src/Component/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Navbar.css";
+
+
+export default function Navbar() {
+  return (
+    <div className="navbarShop">
+      <div className="navbarShopContainer">
+        <div className="navbarLeft">
+          خوش آمدید
+        </div>
+      </div>
+    </div>
+  );
+}
