5432c25f828978903e9aecd1d8d4cb80cb294ea6
Milad Joodi <winbeta.com@gmail.com>
2023-10-14 00:50:29 +0330

    hello new day ♥

--- cms_shop_persian2/src/Components/Comments/Comments.jsx
+++ cms_shop_persian2/src/Components/Comments/Comments.jsx
@@ -4,12 +4,14 @@ import ErrorBox from "../ErrorBox/ErrorBox";
 import DetailsModal from "../DetailsModal/DetailsModal";
 import DeleteModal from "../DeleteModal/DeleteModal";
 import { ToastContainer, toast } from 'react-toastify';
+import EditModal from "../EditModal/EditModal";
 
 export default function Comments() {
 
   const [allCommnets, setAllComments] =useState('')
   const [isShowDetailsModal,setIsShowDetailsModal] = useState(false)
   const [isShowDeleteModal,setIsShowDeleteModal] = useState(false)
+  const [isShowEditModal,setIsShowEditModal] = useState(false)
   const [mainCommentBody,setMainCommentBody] = useState('')
   const [commentID, setCommentID] = useState(null)
 
@@ -34,6 +36,10 @@ export default function Comments() {
     setIsShowDeleteModal(false)
   }
 
+  const closeEditModal = ()=>{
+    setIsShowEditModal(false)
+  }
+
   const deleteComment = ()=>{
     fetch(`http://localhost:8000/api/comments/${commentID}`,{
       method: 'DELETE'
@@ -46,9 +52,15 @@ export default function Comments() {
         toastId: 'success1',
     })
     })
-    
-    
+  
   }
+
+  const updateComment = (event)=>{
+    event.preventDefault()
+    console.log('Edited');
+    closeEditModal()
+  }
+
 
   return (
     <div className="cms-main">
@@ -79,7 +91,10 @@ export default function Comments() {
               setIsShowDeleteModal(true)
               setCommentID(comment.id)
             }}>حذف</button>
-            <button>ویرایش</button>
+            <button onClick={()=>{
+              setIsShowEditModal(true)
+              setMainCommentBody(comment.body)
+            }}>ویرایش</button>
             <button>پاسخ</button>
             <button>تایید</button>
           </td>
@@ -112,6 +127,16 @@ export default function Comments() {
       )}
       <ToastContainer autoClose={2000}  rtl/>
       
+      {isShowEditModal && (
+        <EditModal
+        onClose={setIsShowEditModal}
+        onSubmit={updateComment}
+        >
+          <textarea style={{fontSize: '1.2rem'}} value={mainCommentBody} onChange={(event)=> setMainCommentBody(event.target.value)}>
+            
+          </textarea>
+        </EditModal>
+      )}
     </div>
   );
 }
