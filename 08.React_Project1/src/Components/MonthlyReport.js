20183efb6944122692a38ed6f8c81b6586544f57
Milad Joodi <winbeta.com@gmail.com>
2023-12-19 00:09:50 +0330

    ♥

--- 08.React_Project1/src/Components/MonthlyReport.js
+++ 08.React_Project1/src/Components/MonthlyReport.js
@@ -2,6 +2,140 @@ import React from 'react'
 
 export default function MonthlyReport() {
   return (
-    <div>MonthlyReport</div>
+    <div className="row">
+          <div className="col-md-12">
+            <div className="card">
+              <div className="card-header">
+                <h5 className="card-title">گزارش ماهیانه</h5>
+
+                <div className="card-tools">
+                  <button type="button" className="btn btn-tool" data-widget="collapse">
+                    <i className="fa fa-minus"></i>
+                  </button>
+                  <div className="btn-group">
+                    <button type="button" className="btn btn-tool dropdown-toggle" data-toggle="dropdown">
+                      <i className="fa fa-wrench"></i>
+                    </button>
+                    <div className="dropdown-menu dropdown-menu-left" role="menu">
+                      <a href="#" className="dropdown-item">منو اول</a>
+                      <a href="#" className="dropdown-item">منو دوم</a>
+                      <a href="#" className="dropdown-item">منو سوم</a>
+                      <a className="dropdown-divider"></a>
+                      <a href="#" className="dropdown-item">لینک</a>
+                    </div>
+                  </div>
+                  <button type="button" className="btn btn-tool" data-widget="remove">
+                    <i className="fa fa-times"></i>
+                  </button>
+                </div>
+              </div>
+              {/* <!-- /.card-header --> */}
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-md-8">
+                    <p className="text-center">
+                      <strong>فروش ۱ دی ۱۳۹۷</strong>
+                    </p>
+
+                    <div className="chart">
+                      {/* <!-- Sales Chart Canvas --> */}
+                      <canvas id="salesChart" height="180" style={{height: "180px"}}></canvas>
+                    </div>
+                    {/* <!-- /.chart-responsive --> */}
+                  </div>
+                  {/* <!-- /.col --> */}
+                  <div className="col-md-4">
+                    <p className="text-center">
+                      <strong>میزان پیشرفت اهداف</strong>
+                    </p>
+
+                    <div className="progress-group">
+                      محصولات اضافه شده به سبد خرید
+                      <span className="float-left"><b>160</b>/200</span>
+                      <div className="progress progress-sm">
+                        <div className="progress-bar bg-primary" style={{width: "80%"}}></div>
+                      </div>
+                    </div>
+                    {/* <!-- /.progress-group --> */}
+
+                    <div className="progress-group">
+                      خرید انجام شده
+                      <span className="float-left"><b>310</b>/400</span>
+                      <div className="progress progress-sm">
+                        <div className="progress-bar bg-danger" style={{width: "75%"}}></div>
+                      </div>
+                    </div>
+
+                    {/* <!-- /.progress-group --> */}
+                    <div className="progress-group">
+                      <span className="progress-text">بازدید صفحات ویژه</span>
+                      <span className="float-left"><b>480</b>/800</span>
+                      <div className="progress progress-sm">
+                        <div className="progress-bar bg-success" style={{width: "60%"}}></div>
+                      </div>
+                    </div>
+
+                    {/* <!-- /.progress-group --> */}
+                    <div className="progress-group">
+                      سوالات ارسالی
+                      <span className="float-left"><b>250</b>/500</span>
+                      <div className="progress progress-sm">
+                        <div className="progress-bar bg-warning" style={{width: "50%"}}></div>
+                      </div>
+                    </div>
+                    {/* <!-- /.progress-group --> */}
+                  </div>
+                  {/* <!-- /.col --> */}
+                </div>
+                {/* <!-- /.row --> */}
+              </div>
+              {/* <!-- ./card-body --> */}
+              <div className="card-footer">
+                <div className="row">
+                  <div className="col-sm-3 col-6">
+                    <div className="description-block border-right">
+                      <span className="description-percentage text-success"><i className="fa fa-caret-up"></i> 17%</span>
+                      <h5 className="description-header">تومان 35,210.43</h5>
+                      <span className="description-text">کل گردش حساب</span>
+                    </div>
+                    {/* <!-- /.description-block --> */}
+                  </div>
+                  {/* <!-- /.col --> */}
+                  <div className="col-sm-3 col-6">
+                    <div className="description-block border-right">
+                      <span className="description-percentage text-warning"><i className="fa fa-caret-left"></i> 0%</span>
+                      <h5 className="description-header">تومان 10,390.90</h5>
+                      <span className="description-text">فروش کل</span>
+                    </div>
+                    {/* <!-- /.description-block --> */}
+                  </div>
+                  {/* <!-- /.col --> */}
+                  <div className="col-sm-3 col-6">
+                    <div className="description-block border-right">
+                      <span className="description-percentage text-success"><i className="fa fa-caret-up"></i> 20%</span>
+                      <h5 className="description-header">تومان 24,813.53</h5>
+                      <span className="description-text">سود کل</span>
+                    </div>
+                    {/* <!-- /.description-block --> */}
+                  </div>
+                  {/* <!-- /.col --> */}
+                  <div className="col-sm-3 col-6">
+                    <div className="description-block">
+                      <span className="description-percentage text-danger"><i className="fa fa-caret-down"></i> 18%</span>
+                      <h5 className="description-header">1200</h5>
+                      <span className="description-text">اهداف</span>
+                    </div>
+                    {/* <!-- /.description-block --> */}
+                  </div>
+                </div>
+                {/* <!-- /.row --> */}
+              </div>
+              {/* <!-- /.card-footer --> */}
+            </div>
+            {/* <!-- /.card --> */}
+          </div>
+          {/* <!-- /.col --> */}
+        </div>
+        // <!-- /.row -->
   )
 }
